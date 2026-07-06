d2ee44ede9769dae952e335bf2124906192544bb
Xavier Lopez <xlopez@claflin.com>
2026-07-06 09:16:36 -0400

    chore: remove obsolete and unused test scripts from api/ directory

--- api/test_require.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: application/json");
-$res = [];
-$res['config_exists'] = file_exists(__DIR__ . '/config.php');
-$res['config_readable'] = is_readable(__DIR__ . '/config.php');
-$res['cwd'] = getcwd();
-$res['dir'] = __DIR__;
-echo json_encode($res, JSON_PRETTY_PRINT);
-?>
